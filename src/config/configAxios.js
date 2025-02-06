923ae170b50934bbed3d2d3af6958620a9f6ee86
Julio Vargas Tello <jvdark2020@gmail.com>
2025-02-06 17:21:47 -0500

    Quitanto el TOKEN

--- src/config/configAxios.js
+++ src/config/configAxios.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const configAxios = {
     baseURL: process.env.NEXT_PUBLIC_STRAPI_URL,
-    headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`
-    },
+    // headers: {
+    //     "Content-Type": "application/json",
+    //     "Authorization": `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`
+    // },
 };
 
 export default axios.create(configAxios);
